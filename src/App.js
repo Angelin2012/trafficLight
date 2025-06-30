0499b8f2fbdc95a2b3a6a035d038bb8ef4928d89
Angel <angel.inkeziah14@gmail.com>
2025-06-30 15:50:38 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [light, setLight] = useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Traffic Light</h1>
+      <div className="traffic-light">
+        <div className={`circle red ${light === "red" ? "on" : ""}`}></div>
+        <div className={`circle yellow ${light === "yellow" ? "on" : ""}`}></div>
+        <div className={`circle green ${light === "green" ? "on" : ""}`}></div>
+      </div>
+
+      <div className="buttons">
+        <button onClick={() => setLight("red")}>Red</button>
+        <button onClick={() => setLight("yellow")}>Yellow</button>
+        <button onClick={() => setLight("green")}>Green</button>
+      </div>
     </div>
   );
 }
